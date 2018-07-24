d50e84a9aa31af67b3a7c439c2ba58a167ad17de
chaohsien.iof06g@g2.nctu.edu.tw <chaohsien.iof06g@g2.nctu.edu.tw>
2018-07-24 12:34:33 +0800

    Week3

--- Week3/ViewController.swift
+++ Week3/ViewController.swift
@@ -15,13 +15,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var account: UITextField!    // 輸入帳號
     @IBOutlet weak var password: UITextField!   // 輸入密碼
     
+    let line: UILabel = UILabel()
+    let blank: UITextView = UITextView()
+    
     let myAccount = "luke@gmail.com"
     let myPassword = "1234"
     
+    let alertController = UIAlertController(title: "Error", message: "Login fail.", preferredStyle: .alert)
+    let action1 = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+        print("You've wrong ID");
+    }
+    
     @IBAction func mySegmentedAction(_ sender: UISegmentedControl) {
-        
-        let line: UILabel = UILabel()
-        let blank: UITextView = UITextView()
         
         if sender.selectedSegmentIndex == 1 {   //判斷使用者選擇是哪一個Segmented 0是log in 、1是sign up
            line.isHidden = false
@@ -29,6 +34,7 @@ class ViewController: UIViewController {
         } //else {
             //blank.isHidden = false      // check欄需可使用
         //}
+        
     }
     
     
@@ -43,11 +49,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func login(_ sender: UIButton) {
+    @IBAction func login(_ sender: UIButton ) {
         
         if account.text == myAccount && password.text == myPassword {
             print("成功")
-            // self.init.text = "成功"
             
             //傳至下一頁面
             let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "loginView")
@@ -56,8 +61,8 @@ class ViewController: UIViewController {
             // 返回 self.dismissViewControllerAnimated(true, completion: nil)
             
         } else{
-            print("輸入錯誤")
-           // self.init.text = "輸入錯囉"
+            alertController.addAction(action1)
+            self.present(alertController, animated: true, completion: nil )
         }
         
     }
