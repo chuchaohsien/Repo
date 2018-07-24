53d9ce0ee4f372c189671344bb710948e235ae26
chaohsien.iof06g@g2.nctu.edu.tw <chaohsien.iof06g@g2.nctu.edu.tw>
2018-07-24 10:13:08 +0800

    Week3

--- Week3/ViewController.swift
+++ Week3/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mycheck: UILabel!
+    @IBOutlet weak var thirdtext: UITextField!
+    
+    @IBAction func mySegmentedAction(_ sender: UISegmentedControl) {
+        
+        let line: UILabel = UILabel()
+        let blank: UITextView = UITextView()
+        
+        if sender.selectedSegmentIndex == 1 {   //判斷使用者選擇是哪一個Segmented 0是log in 、1是sign up
+           line.isHidden = false
+           blank.isHidden = false         // check欄需可使用
+        } //else {
+            //blank.isHidden = false      // check欄需可使用
+        //}
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
